037b50ada658dcdc3560f803a24da2d88ecca3dd
Andres <anondiaz@gmail.com>
2025-04-15 11:20:30 +0200

    First commit

---
+++ ejercicios_sakila_1.sql
@@ -0,0 +1,86 @@
+-- Utiliza la base de datos sakila, disponible en MySQL Workbench,
+-- para resolver estos ejercicios 
+
+
+
+
+-- 1) Actores que tienen el primer nombre "Gary"
+
+
+
+-- 2) Actores que tiene de primer apellido "Streep"
+
+
+
+-- 3) Actores que contengan una "o" en su nombre
+
+
+
+-- 4) Actores que contengan una "a" en su nombre y una "e" en su apellido
+
+
+
+-- 5) Actores que contengan dos "o" en su nombre (en cualquier posicion) y una "a" en su apellido
+
+
+
+-- 6) Actores cuya tercera letra del nombre sea "b"
+
+
+-- 7) Ciudades que empiezan por "a"
+
+
+
+-- 8) Ciudades que acaban por "s"
+
+
+
+-- 9) Ciudades del country "France"
+
+
+
+-- 10) Ciudades con nombres compuestos (como New York)
+
+
+
+
+-- 11) películas con una duración entre 80 y 100 m.
+
+
+
+
+-- 12) películas con un rental_rate entre 1 y 3
+
+
+
+-- 13) películas con un título de más de 11 letras.
+
+
+
+-- 14) películas con un rating de PG o G.
+
+
+ 
+-- 15) ¿Cuantas ciudades tiene el country ‘France’? 
+
+
+
+-- 16) Películas que no tengan un rating de NC-17
+
+
+
+-- 17) Películas con un rating PG y duración de más de 120.
+
+
+
+-- 18) ¿Cuantos actores hay?
+
+
+
+-- 19) Película con mayor duración.
+
+
+
+-- 20) ¿Cuantos clientes viven en Indonesia?
+
+
